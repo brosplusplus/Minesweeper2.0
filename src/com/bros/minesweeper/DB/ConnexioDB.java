a03802652985179e3373aeedb426b149c1b7a0e2
alaxmo13 <alaxmo13@PCAlex>
2015-01-13 10:55:41 +0100

    updated files

---
+++ src/com/bros/minesweeper/DB/ConnexioDB.java
@@ -0,0 +1,22 @@
+package com.bros.minesweeper.DB;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class ConnexioDB {
+	
+	private static String dbURL = "jdbc:derby://localhost:1527/HibernateDB;create=true";
+    private static Connection conn = null;
+	
+    public static void createConnection() {
+        try {
+            Class.forName("org.apache.derby.jdbc.ClientDriver").newInstance();
+            //Get a connection
+            conn = DriverManager.getConnection(dbURL); 
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    
+}
