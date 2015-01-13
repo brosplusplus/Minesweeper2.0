237d771c516fdfc59980d537798cb7a807df40f2
Borja <borja.arias.upc@gmail.com>
2015-01-13 19:21:01 +0100

    Afegit PersistenceSessionFactory

--- src/com/bros/minesweeper/test/CasellaTest.java
+++ src/com/bros/minesweeper/test/CasellaTest.java
@@ -1,29 +1,19 @@
 package com.bros.minesweeper.test;
 
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.AnnotationConfiguration;
-import org.hibernate.classic.Session;
-import org.hibernate.tool.hbm2ddl.SchemaExport;
 
+import com.bros.minesweeper.db.PersistenceSessionFactory;
 import com.bros.minesweeper.domain.model.Casella;
 import com.bros.minesweeper.domain.model.Partida;
 
 public class CasellaTest {
 	public static void main (String[] args) {
-		AnnotationConfiguration conf = new AnnotationConfiguration();
-		conf.addAnnotatedClass(Casella.class);
-		conf.configure("hibernate.cfg.xml");
-		new SchemaExport(conf).create(true, true);
-		SessionFactory sessionfact = conf.buildSessionFactory();
-		Session session = sessionfact.getCurrentSession();
+		Session session = PersistenceSessionFactory.getInstance().openSession();
 		session.beginTransaction();
-		Casella c = new Casella();
-		c.setNumeroColumna(1);
-		c.setNumeroFila(2);
 		Partida p = new Partida();
-		c.setPartida(p);
-		session.save(p);
-		session.save(c);
+		Casella c = new Casella(1,1,p);
 		session.getTransaction().commit();
 		session.close();
 	}
--- src/com/bros/minesweeper/test/UsuariRegistratTest.java
+++ src/com/bros/minesweeper/test/UsuariRegistratTest.java
@@ -15,26 +15,26 @@ public class UsuariRegistratTest {
 	public static void main (String[] args) {
 		AnnotationConfiguration conf = new AnnotationConfiguration();
 		conf.configure("hibernate.cfg.xml");
-		new SchemaExport(conf).create(true, true);
+		//new SchemaExport(conf).create(true, true);
 		SessionFactory sessionfact = conf.buildSessionFactory();
 		Session session = sessionfact.getCurrentSession();
 		
 		
-		List<Administrador> l = session.createSQLQuery("select * from test.ADMINISTRADOR a,"
-				+ " test.USUARIREGISTRAT u where u.username = :username").
-				.addEntity("com.bros.minesweeper.domain.model.Administrador")
-				.list();
-		for (int i = 0; i < l.size(); i++)
-		{
-			Administrador ad = l.get(i);
-			System.out.println(ad.getUsername());
-		}
-//		session.beginTransaction();
-//		Jugador u = new Jugador();
-//		u.setNom("Brojs");
-//		u.setEmail("email.@email.com");
-//		u.setUsername("nuevo user");
-//		session.save(u);
-//		session.getTransaction().commit();
+//		List<Administrador> l = session.createSQLQuery("select * from test.ADMINISTRADOR a,"
+//				+ " test.USUARIREGISTRAT u where u.username = :username")
+//				.addEntity("com.bros.minesweeper.domain.model.Administrador")
+//				.list();
+//		for (int i = 0; i < l.size(); i++)
+//		{
+//			Administrador ad = l.get(i);
+//			System.out.println(ad.getUsername());
+//		}
+		session.beginTransaction();
+		Jugador u = new Jugador();
+		u.setNom("Brojs");
+		u.setEmail("email.@email.com");
+		u.setUsername("nueddvwqfqso user");
+		session.save(u);
+		session.getTransaction().commit();
 	}
 }
