b59eb70ad09db2759129f53ff3403eaf2b9b154e
ShakMR <borja.arias.upc@gmail.com>
2015-01-13 13:53:11 +0100

    Controlador Casella

--- src/com/bros/minesweeper/db/CtrlCasella.java
+++ src/com/bros/minesweeper/db/CtrlCasella.java
@@ -3,7 +3,18 @@
  */
 package com.bros.minesweeper.db;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.List;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.AnnotationConfiguration;
+import org.hibernate.classic.Session;
+import org.hibernate.tool.hbm2ddl.SchemaExport;
+
 import com.bros.minesweeper.datainterface.ICtrlCasella;
+import com.bros.minesweeper.domain.model.Administrador;
 import com.bros.minesweeper.domain.model.Casella;
 
 /**
@@ -11,11 +22,36 @@ import com.bros.minesweeper.domain.model.Casella;
  * @author Borja Arias
  */
 public class CtrlCasella implements ICtrlCasella{
-
+	
+	private static String tableName = "Casella";
+    // jdbc Connection
+    private static Connection conn = null;
+    private static Statement stmt = null;
+	
 	@Override
 	public Casella get(Integer idPartida, Integer numF, Integer numC) {
-		// TODO Auto-generated method stub
+		try {
+			//ConnexioDB.createConnection();
+			AnnotationConfiguration conf = new AnnotationConfiguration();
+			conf.configure("hibernate.cfg.xml");
+			SessionFactory sessionfact = conf.buildSessionFactory();
+			Session session = sessionfact.getCurrentSession();
+			Casella cas = (Casella)session.createSQLQuery("select * from test.Casella c "
+					+ "where  c.idPartida = "+idPartida
+					+ "       c.numeroFila = "+numF
+					+ "       c.numeroColumna = "+numC)
+					.addEntity("com.bros.minesweeper.domain.model.Casella")
+					.uniqueResult();
+			return cas;
+		}
+		catch (Exception e) {
+            e.printStackTrace();
+		}
 		return null;
 	}
 	
+	
+	public static void main (String[] args) {
+		
+	}
 }
