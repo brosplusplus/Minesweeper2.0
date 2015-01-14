c76d5b8caaf498d31eb80621a1ffd48cf418245f
Axelred <alex.martinez.romero@gmail.com>
2015-01-14 12:05:46 +0100

    trdyufy

---
+++ src/com/bros/minesweeper/presentation/PanelInici.java
@@ -0,0 +1,37 @@
+package com.bros.minesweeper.presentation;
+
+import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+import javax.swing.Box;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import java.awt.Color;
+
+public class PanelInici extends JPanel {
+
+	/**
+	 * Create the panel.
+	 */
+	public PanelInici() {
+		setForeground(Color.LIGHT_GRAY);
+		GroupLayout groupLayout = new GroupLayout(this);
+		groupLayout.setHorizontalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGap(0, 450, Short.MAX_VALUE)
+		);
+		groupLayout.setVerticalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGap(0, 300, Short.MAX_VALUE)
+		);
+		setLayout(groupLayout);
+
+	}
+
+}
