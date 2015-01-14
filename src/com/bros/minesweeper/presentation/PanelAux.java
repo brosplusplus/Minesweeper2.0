9eee2aa739cbde11c2e5494dc3ebaeac921ccfda
Axelred <alex.martinez.romero@gmail.com>
2015-01-14 13:17:29 +0100

    some changes

--- src/com/bros/minesweeper/presentation/PanelAux.java
+++ src/com/bros/minesweeper/presentation/PanelAux.java
@@ -1,49 +1,62 @@
 package com.bros.minesweeper.presentation;
 
-import javax.swing.JPanel;
-import javax.swing.GroupLayout;
-import javax.swing.GroupLayout.Alignment;
-import javax.swing.JLabel;
-
-import java.awt.Font;
-
-import javax.swing.Box;
-
 import java.awt.Component;
 import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
+import javax.swing.Box;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
 import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
-public class PanelInici extends JPanel {
+import java.awt.Color;
 
+public class PanelAux extends JPanel {
+
+	private JugarPartidaView JPV;
 	/**
 	 * Create the panel.
 	 * @param JPV 
 	 */
-	public PanelInici(final JugarPartidaView JPV) {
+	public PanelAux(final JugarPartidaView JPV) {
+		
+		this.JPV = JPV;
+		
+		setForeground(Color.LIGHT_GRAY);
+		GroupLayout groupLayout = new GroupLayout(this);
+		groupLayout.setHorizontalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGap(0, 450, Short.MAX_VALUE)
+		);
+		groupLayout.setVerticalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGap(0, 300, Short.MAX_VALUE)
+		);
+		setLayout(groupLayout);
 		
 		Box verticalBox = Box.createVerticalBox();
-		
 		Component verticalGlue = Box.createVerticalGlue();
 		verticalBox.add(verticalGlue);
 		
-		JLabel label = new JLabel("Minesweeper");
-		label.setFont(new Font("Dialog", Font.BOLD, 42));
-		label.setAlignmentX(0.5f);
-		verticalBox.add(label);
+		JLabel lblTitle = new JLabel("Minesweeper");
+		lblTitle.setFont(new Font("Comic Sans MS", Font.BOLD, 42));
+		lblTitle.setAlignmentX(Component.CENTER_ALIGNMENT);
+		verticalBox.add(lblTitle);
 		
 		Component verticalStrut = Box.createVerticalStrut(20);
 		verticalBox.add(verticalStrut);
 		
-		JLabel label_1 = new JLabel("Bros++ ® 2014");
-		label_1.setFont(new Font("Dialog", Font.PLAIN, 18));
-		label_1.setAlignmentX(0.5f);
-		verticalBox.add(label_1);
+		JLabel lblCompany = new JLabel("Bros++ \u00AE 2014");
+		lblCompany.setFont(new Font("Arial", Font.PLAIN, 18));
+		lblCompany.setAlignmentX(Component.CENTER_ALIGNMENT);
+		verticalBox.add(lblCompany);
 		
-		Component verticalGlue_1 = Box.createVerticalGlue();
-		verticalBox.add(verticalGlue_1);
+		Component verticalGlue_2 = Box.createVerticalGlue();
+		verticalBox.add(verticalGlue_2);
 		
 		Box horizontalBox = Box.createHorizontalBox();
 		verticalBox.add(horizontalBox);
@@ -66,14 +79,15 @@ public class PanelInici extends JPanel {
 		btnExit.setMnemonic('s');
 		horizontalBox.add(btnExit);
 		
-		Component horizontalGlue = Box.createHorizontalGlue();
-		horizontalBox.add(horizontalGlue);
+		Component horizontalGlue_2 = Box.createHorizontalGlue();
+		horizontalBox.add(horizontalGlue_2);
 		
 		JButton btnEntry = new JButton("Entrar");
 		btnEntry.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseReleased(MouseEvent e) {
-				JPV.mostrarIdentificacio();
+				identificacio.main(null);
+				JPV.getJPVC().PrJugar();
 			}
 		});
 		btnEntry.setFont(new Font("Arial", Font.PLAIN, 14));
@@ -90,20 +104,7 @@ public class PanelInici extends JPanel {
 		
 		JPanel panel = new JPanel();
 		verticalBox.add(panel);
-		GroupLayout groupLayout = new GroupLayout(this);
-		groupLayout.setHorizontalGroup(
-			groupLayout.createParallelGroup(Alignment.LEADING)
-				.addGroup(groupLayout.createSequentialGroup()
-					.addGap(12)
-					.addComponent(verticalBox, GroupLayout.PREFERRED_SIZE, 450, GroupLayout.PREFERRED_SIZE))
-		);
-		groupLayout.setVerticalGroup(
-			groupLayout.createParallelGroup(Alignment.LEADING)
-				.addGroup(groupLayout.createSequentialGroup()
-					.addGap(12)
-					.addComponent(verticalBox, GroupLayout.PREFERRED_SIZE, 300, GroupLayout.PREFERRED_SIZE))
-		);
-		setLayout(groupLayout);
 
 	}
+
 }
