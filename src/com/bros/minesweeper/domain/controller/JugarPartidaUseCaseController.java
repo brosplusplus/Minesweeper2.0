d5d05356a5159f6d490d4b6a4ff450dbd23e126d
alaxmo13 <alaxmo13@hotmail.com>
2015-01-15 10:52:17 +0100

    domain controller package commented.

--- src/com/bros/minesweeper/domain/controller/JugarPartidaUseCaseController.java
+++ src/com/bros/minesweeper/domain/controller/JugarPartidaUseCaseController.java
@@ -8,8 +8,12 @@ import com.bros.minesweeper.domain.model.EstatPartida;
 import com.bros.minesweeper.domain.model.Jugador;
 import com.bros.minesweeper.domain.model.Partida;
 import com.bros.minesweeper.factory.FactoriaControladors;
-import com.bros.minesweeper.utils.debug;
 
+/**
+ *  JugarPartidaUseCaseController representa el cas d'us de Jugar Partida
+ * @author Alex More
+ *
+ */
 public class JugarPartidaUseCaseController {
 
 	/**
@@ -35,7 +39,7 @@ public class JugarPartidaUseCaseController {
 	}
 	
 	/**
-	 * Methods
+	 * Crea una partida amb el Nivell nomnivell.
 	 * @throws IllegalAccessException 
 	 * @throws InstantiationException 
 	 * @throws Exception 
@@ -45,19 +49,43 @@ public class JugarPartidaUseCaseController {
 		this.partida = new Partida(jugName, nivell);
 	}
 	
-	
+	/**
+	 * Marquem una casella del taulell.
+	 * @param numF fila de la casella.
+	 * @param numC columna de la casella.
+	 * @throws Exception
+	 */
 	public void marcarCasella (int numF, int numC) throws Exception {
 		this.partida.marcarCasella(numF, numC);
 	}
 	
+	/**
+	 * Desmarquem una casella del taulell.
+	 * @param numF fila de la casella.
+	 * @param numC columna de la casella.
+	 * @throws Exception
+	 */
 	public void desmarcarCasella (int numF, int numC) throws Exception {
 		this.partida.desmarcarCasella(numF, numC);
 	}
 	
+	/**
+	 * Descobrim una casella del taulell.
+	 * @param numF
+	 * @param numC
+	 * @return retorna un EstatPartida amb la situacio actual de la partida.
+	 * @throws Exception
+	 */
 	public EstatPartida descobrirCasella (int numF, int numC) throws Exception {
 		return this.partida.descobrirCasella(numF, numC);
 	}
 	
+	/**
+	 * Funcio per validar l'acces d'un usuari al sistema.
+	 * @param userN nom d'usuari.
+	 * @param passwd contrasenya de l'usuari.
+	 * @throws Exception
+	 */
 	public void FerAutenticacio(String userN,String passwd) throws Exception {
 		CtrlJugador cj = FactoriaControladors.getCtrlJugador();
 		if (!cj.exists(userN)) throw new Exception("L'usuari " + userN + " no es jugador");
@@ -66,6 +94,11 @@ public class JugarPartidaUseCaseController {
 		jugName = cj.get(userN);
 	}
 	
+	/**
+	 * Funcio per obtenir tots els nivells de la DB.
+	 * @return retorna una llista amb els nivells.
+	 * @throws Exception
+	 */
 	public ArrayList<HashMap<String, String> > obtenirNivells() throws Exception {
 		ConsultarNivellUseCaseController cnucc = new ConsultarNivellUseCaseController();
 		return cnucc.consultarNivells();
--- src/com/bros/minesweeper/domain/controller/LoginUseCaseController.java
+++ src/com/bros/minesweeper/domain/controller/LoginUseCaseController.java
@@ -13,6 +13,13 @@ import com.bros.minesweeper.factory.FactoriaControladors;
 
 public class LoginUseCaseController {
 	
+	/**
+	 * Funció per comprovar que les dades de l'usuari son correctes i estan a la DB.
+	 * @param userN nom d'usuari.
+	 * @param passwd contrasenya.
+	 * @return retorna true si l'usuari existeix i el userN i el pwd son correctes, false altrament.
+	 * @throws Exception
+	 */
 	public Boolean login (String userN, String passwd) throws Exception {
 		CtrlUsuariRegistrat cu = FactoriaControladors.getCtrlUsuariRegistrat();
 		UsuariRegistrat u = cu.get(userN);
