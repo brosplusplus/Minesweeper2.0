460da66d97a87db2f5ba1ed45562d23a04b5c53c
alaxmo13 <alaxmo13@hotmail.com>
2015-01-15 11:21:03 +0100

    and more xD

--- src/com/bros/minesweeper/factory/FactoriaAdaptadorCorreu.java
+++ src/com/bros/minesweeper/factory/FactoriaAdaptadorCorreu.java
@@ -2,6 +2,11 @@ package com.bros.minesweeper.factory;
 
 import com.bros.minesweeper.adapter.AdaptadorCorreuPropi;
 
+/**
+ * Factoria de l'Adaptador de correu propi.
+ * @author Alex More
+ *
+ */
 public class FactoriaAdaptadorCorreu extends AbstractFactory {
 	
 	public static AdaptadorCorreuPropi getAdaptadorCorreuPropi()
